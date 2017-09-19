0a49d02f6757549dbb437d433400b18b38e74425
Hans Bentlöv <hans.bentlov@hm.com>
2017-09-19 12:38:01 +0200

    Add & remove some applications

--- brew-cask.sh
+++ brew-cask.sh
@@ -6,34 +6,30 @@
 
 
 # Install native apps
+brew tap caskroom/cask
 
-brew install caskroom/cask/brew-cask
-brew tap caskroom/versions
-
-# daily
-brew cask install spectacle
+# Daily-used applications
+brew cask install spotify
+brew cask install visual-studio-code
+brew cask install sourcetree
+brew cask install evernote
 brew cask install gyazo
 brew cask install flux
 
-# dev
 brew cask install iterm2
 brew cask install imagealpha
 brew cask install imageoptim
 
 # browsers
-brew cask install google-chrome-canary
+# brew cask install google-chrome-canary
 brew cask install google-chrome
 # brew cask install firefoxnightly
 # brew cask install webkit-nightly
 # brew cask install chromium
 # brew cask install torbrowser
 
+
+# Other
 brew cask install disk-inventory-x
 brew cask install vlc
 brew cask install utorrent
-brew cask install evernote
-
-brew cask install spotify
-
-brew tap caskroom/fonts 
-brew cask install font-fira-code
