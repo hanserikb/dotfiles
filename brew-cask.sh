c256da9b3169c1ea0cd70456554518a4ff305708
Hans Bentlöv <hanserikb@gmail.com>
2019-04-30 15:52:45 +0200

    Update brew-cask.sh

--- brew-cask.sh
+++ brew-cask.sh
@@ -17,7 +17,7 @@ brew cask install sourcetree # git gui
 brew cask install spotify # music
 brew cask install evernote # note-taking
 brew cask install gyazo # screenshotting
-brew cask install flux # screen-dimming
+#brew cask install flux # screen-dimming
 brew cask install slack # communication
 brew cask install iterm2 # terminal
 brew cask install imagealpha # image minifier
@@ -25,14 +25,14 @@ brew cask install imageoptim # fast image optimiser
 
 # Browsers
 # brew cask install google-chrome-canary
-brew cask install google-chrome
+# brew cask install google-chrome
 # brew cask install firefoxnightly
 # brew cask install webkit-nightly
 # brew cask install chromium
 # brew cask install torbrowser
 
 # Other
-brew cask install disk-inventory-x # where did my diskspace go?
+#brew cask install disk-inventory-x # where did my diskspace go?
 brew cask install vlc # video player
-brew cask install utorrent # torrents
+#brew cask install utorrent # torrents
 brew cask install fliqlo # screensaver
